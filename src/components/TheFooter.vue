b0705cbaa08eeaa20247c8b62ff61544013c11e9
Francesco Rocco <francescorocco97@gmail.com>
2023-03-27 16:17:55 +0200

    Header aggiunto, lavorando sul footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,95 @@
+<script>
+export default {
+  name: "TheFooter",
+  data() {
+    return {
+        foorterLiks:[
+            {
+                name: "DC COMICS",
+                links:[
+                    "Characters",
+                    "Comics",
+                    "Movies",
+                    "TV",
+                    "Games",
+                    "Videos",
+                    "News"
+                ]
+            },
+            {
+                name: "SHOP",
+                links: [
+                    "Shop DC",
+                    "Shop DC Collectibles"
+                ]
+            },
+            {
+                name: "DC",
+                links: [
+                    "Terms Of Use",
+                    "Privacy policy(New)",
+                    "Ad Choices",
+                    "Advertising",
+                    "Jobs",
+                    "Subscriptions",
+                    "Talent Workshops",
+                    "CPSC Certificates",
+                    "Ratings",
+                    "Shop Help",
+                    "Contact Us"
+                ]
+            },
+            {
+                name: "SITES",
+                links: [
+                    "DC",
+                    "MAD Magazine",
+                    "DC Kids",
+                    "DC Universe",
+                    "DC Power Vista"
+                ]
+            }
+        ]
+    }
+}}
+</script>
+
+
+<template>
+    <footer>
+        <ul class="links-wrapper">
+            <li v-for="(element, index) in foorterLiks">
+                {{element.name}}
+                <ul>
+                    <li v-for="link in foorterLiks.links">{{ link }}</li>
+                </ul>
+            </li>
+        </ul>
+    </footer>
+</template>
+
+
+
+<style scoped lang="scss">
+@use "../style/partials/mixins";
+
+    footer{
+        background-image: url(../assets/footer-bg.jpg);
+        height: 370px;
+        padding: 20px 50px;
+    }
+
+    .links-wrapper{
+        @include mixins.d-flex;
+        list-style: none;
+        
+
+        li{
+            color: white;
+            padding: 0 25px 35px;
+            font-weight: bolder;
+            font-size: 25px;
+        }
+    }
+
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  name: "TheHeader",
+  data() {
+    return {
+        navbarLinks:[
+            "CHARACTERS",
+            "COMICS",
+            "MOVIES",
+            "TV",
+            "GAMES",
+            "CELECTIBLES",
+            "VIDEOS",
+            "FANS",
+            "NEWS",
+            "SHOP"
+
+        ]
+    }
+}}
+</script>
+
+<template>
+
+<header>
+    <div>
+        <img src="../assets/dc-logo.png" alt=" ">
+    </div>
+    <nav>
+        <ul>
+            <li v-for="element in navbarLinks">
+                <a class="nav-links" href="#">{{element}}</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
+</template>
+
+
+<style scoped lang="scss">
+@use "../style/partials/mixins";
+
+header{
+    @include mixins.d-flex-a-center;
+    justify-content: space-evenly;
+    height: 170px;
+}
+
+ul{
+    @include mixins.d-flex;
+    list-style: none;
+    padding: 10px 20px;
+
+    .nav-links{
+        color: black;
+        text-decoration: none;
+        padding: 10px 20px;
+        font-weight: 600;
+    }
+}
+</style>
